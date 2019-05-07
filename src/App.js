84b303dd5e7e3e763ad99db7856e70c9163dd96f
redwanrahat <rahat.lx@gmail.com>
2019-05-08 01:31:07 +0600

    called the geolocation api function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    err => console.log(err)
+  )
   return (
     <div>
     </div>
