079a1d918fb44d7d7b54437e810a890c02439424
redwanrahat <rahat.lx@gmail.com>
2019-05-08 03:10:31 +0600

    refectored state & used componentDidMount lifecycle method

--- src/App.js
+++ src/App.js
@@ -2,34 +2,37 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      lat: null,
-      errorMessage: ''
-    }
+  state = {
+    lat: null,
+    errorMessage: ''
+  }
+  componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({lat: position.coords.latitude})
-      },
-      err => {
-        this.setState({errorMessage: err.message})
-      }
+      position => this.setState({lat: position.coords.latitude}),
+      err => this.setState({errorMessage: err.message})
     )
   }
   render(){
     if(this.state.errorMessage && !this.state.lat){
       return(
-        <div>Error: {this.state.errorMessage}</div>
+        <div>
+          Error:
+          {this.state.errorMessage}
+        </div>
       )
     }
     if(!this.state.errorMessage && this.state.lat){
       return(
-        <div>Latitude: {this.state.lat}</div>
+        <div>
+          Latitude: 
+          {this.state.lat}
+        </div>
       )
     }
     return(
-      <div>Loading!</div>
+      <div>
+        Loading!
+      </div>
     )
   }
 }
